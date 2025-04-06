a20c268fa03988527be85871265938c1c3840da5
Svitlana Midianko <s.midianko@gmail.com>
2025-04-06 13:41:50 -0700

    env var handling

--- src/config.js
+++ src/config.js
@@ -1,9 +1,12 @@
 // Backend API configuration
 const isStaging = process.env.REACT_APP_USE_STAGING === 'true';
+console.log('Environment variable REACT_APP_USE_STAGING:', process.env.REACT_APP_USE_STAGING);
+console.log('isStaging:', isStaging);
 
 export const API_BASE_URL = isStaging 
   ? 'https://isee-api-staging-twilight-grass-3635.fly.dev'
   : 'http://localhost:7777';
+console.log('API_BASE_URL:', API_BASE_URL);
 
 // API endpoints
 export const API_ENDPOINTS = {
